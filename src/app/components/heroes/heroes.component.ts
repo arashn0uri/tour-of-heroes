fa3ca86b5ac67e870d105634d8d9d2137284b6d9
arashnu0ri <arash.nouri@engimtorino.net>
2021-03-29 11:31:53 +0200

    version 1.0.0

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/models/hero';
 import { HeroService } from 'src/app/services/hero.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -10,6 +10,8 @@ import { Observable } from 'rxjs';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
+
+  // heroes$ : Observable<Hero[]> = of([]);
   constructor(
     private heroService: HeroService
     ) { }
@@ -19,6 +21,22 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     let observable : Observable<Hero[]> = this.heroService.getHeroes();
     observable.subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService.deleteHero(hero.id)
+    .subscribe(() => {
+      this.heroes = this.heroes.filter(h => h !== hero);
+    });
   }
 
   ngOnInit(): void {
